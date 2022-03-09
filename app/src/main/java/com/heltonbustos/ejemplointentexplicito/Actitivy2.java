42041c06480b9508553c763990d77ac77cf18e41
Helton Bustos <heltonsmith@hotmail.com>
2022-03-09 19:39:35 -0300

    Paso de parametros entre Intent putExtra getStringExtra

--- app/src/main/java/com/heltonbustos/ejemplointentexplicito/Actitivy2.java
+++ app/src/main/java/com/heltonbustos/ejemplointentexplicito/Actitivy2.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Actitivy2 extends AppCompatActivity {
 
     Button btnIr2;
+    TextView txtUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,12 @@ public class Actitivy2 extends AppCompatActivity {
         setContentView(R.layout.activity_actitivy2);
 
         btnIr2 = findViewById(R.id.btnIr2);
+        txtUsuario = findViewById(R.id.txtUsuario);
+
+        String nombre = getIntent().getStringExtra("nom");
+        String apellido = getIntent().getStringExtra("ape");
+
+        txtUsuario.setText("Bienvenido: " + nombre + " " + apellido);
 
         btnIr2.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/heltonbustos/ejemplointentexplicito/MainActivity.java
+++ app/src/main/java/com/heltonbustos/ejemplointentexplicito/MainActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnIr1;
+    EditText txtNombre, txtApellido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +19,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnIr1 = findViewById(R.id.btnIr1);
+        txtNombre = findViewById(R.id.txtNombre);
+        txtApellido = findViewById(R.id.txtApellido);
 
         btnIr1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String nombre = txtNombre.getText().toString();
+                String apellido = txtApellido.getText().toString();
+
                 Intent intent = new Intent(getApplicationContext(), Actitivy2.class);
+                intent.putExtra("nom", nombre);
+                intent.putExtra("ape", apellido);
+
                 startActivity(intent);
             }
         });
